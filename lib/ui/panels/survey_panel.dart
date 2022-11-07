4d894f8d8d598f783f770e7d4df50bec85522f8e
Stephen Hurwit <sjhurwit@gmail.com>
2022-11-07 16:16:01 -0600

    fix time format,

--- lib/ui/panels/survey_panel.dart
+++ lib/ui/panels/survey_panel.dart
@@ -88,9 +88,12 @@ class _SurveyPanelState extends State<SurveyPanel> {
               ),
             ),
           )),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
-            child: SurveyWidget.buildContinueButton(_surveyController),
+          Visibility(
+            visible: widget.inputEnabled,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+              child: SurveyWidget.buildContinueButton(_surveyController),
+            ),
           ),
         ],
     ));
