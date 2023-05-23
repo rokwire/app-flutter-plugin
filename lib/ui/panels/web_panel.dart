8bc976838dbd4667489e20bd42d088e2b12455f6
Stephen Hurwit <sjhurwit@gmail.com>
2023-05-23 10:34:37 -0500

    upgrade dependencies, fix errors

--- lib/ui/panels/web_panel.dart
+++ lib/ui/panels/web_panel.dart
@@ -88,7 +88,9 @@ class WebPanel extends StatefulWidget {
     if (title != null) {
       contentList.add(flutter_html.Html(data: title,
           onLinkTap: (url, context, attributes, element) => onTapStatusLink(url),
-          style: { "body": flutter_html.Style(color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.bold, fontSize: const flutter_html.FontSize(32), textAlign: TextAlign.center, padding: EdgeInsets.zero, margin: EdgeInsets.zero), },),
+          style: { "body": flutter_html.Style(color: Styles().colors!.fillColorPrimary,
+              fontFamily: Styles().fontFamilies!.bold, fontSize: flutter_html.FontSize(32),
+              textAlign: TextAlign.center, padding: EdgeInsets.zero, margin: flutter_html.Margins.zero), },),
       );
     }
 
@@ -99,7 +101,9 @@ class WebPanel extends StatefulWidget {
     if ((message != null)) {
       contentList.add(flutter_html.Html(data: message,
         onLinkTap: (url, context, attributes, element) => onTapStatusLink(url),
-        style: { "body": flutter_html.Style(color: Styles().colors!.fillColorPrimary, fontFamily: Styles().fontFamilies!.regular, fontSize: const flutter_html.FontSize(20), textAlign: TextAlign.left, padding: EdgeInsets.zero, margin: EdgeInsets.zero), },),
+        style: { "body": flutter_html.Style(color: Styles().colors!.fillColorPrimary,
+            fontFamily: Styles().fontFamilies!.regular, fontSize: flutter_html.FontSize(20),
+            textAlign: TextAlign.left, padding: EdgeInsets.zero, margin: flutter_html.Margins.zero), },),
       );
     }
 
