81cee2b90bc2c45c4edcbec0d359f9da0080ca88
Stephen Hurwit <sjhurwit@gmail.com>
2023-08-22 15:18:54 -0500

    fix storage import capitalization in config.dart

--- lib/service/config.dart
+++ lib/service/config.dart
@@ -26,7 +26,7 @@ import 'package:rokwire_plugin/service/log.dart';
 import 'package:rokwire_plugin/service/notification_service.dart';
 import 'package:rokwire_plugin/service/service.dart';
 import 'package:package_info/package_info.dart';
-import 'package:rokwire_plugin/service/Storage.dart';
+import 'package:rokwire_plugin/service/storage.dart';
 import 'package:rokwire_plugin/service/network.dart';
 import 'package:rokwire_plugin/utils/utils.dart';
 import 'package:path/path.dart';
