c667135254c356e13e5a757720f601a30376ebb1
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2023-09-13 11:28:10 +0300

    Do not lower the case for the formatted date time.

--- lib/service/app_datetime.dart
+++ lib/service/app_datetime.dart
@@ -135,7 +135,6 @@ class AppDateTime with Service {
           timezone.TZDateTime? tzDateTime = (uniLocation != null) ? timezone.TZDateTime.from(dateTime, uniLocation) : null;
           formattedDateTime = (tzDateTime != null) ? dateFormat.format(tzDateTime) : null;
       }
-      formattedDateTime = formattedDateTime?.toLowerCase();
       if (showTzSuffix && (formattedDateTime != null)) {
         formattedDateTime = '$formattedDateTime CT';
       }
