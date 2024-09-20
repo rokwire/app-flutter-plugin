f2728370cb52c612749d1681251548efb778e123
Mihail Varbanov <misho@inabyte.com>
2024-09-20 13:12:11 +0300

    Log more meaningful data on firebase message arrival [rokwire/illinois-app#4256].

--- lib/service/firebase_messaging.dart
+++ lib/service/firebase_messaging.dart
@@ -120,7 +120,7 @@ class FirebaseMessaging with Service {
 
   @protected
   Future<dynamic> onFirebaseMessage(firebase_messaging.RemoteMessage message) async {
-    Log.d("FCM: onFirebaseMessage: $message");
+    Log.d("FCM: onFirebaseMessage: ${message.data}");
     try {
       if ((AppLivecycle.instance?.state == AppLifecycleState.resumed) && StringUtils.isNotEmpty(message.notification?.body)) {
         NotificationService().notify(notifyForegroundMessage, {
