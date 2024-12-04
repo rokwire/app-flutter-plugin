9f77c7e6b12a97aef5c67581256659feeb273a57
Mihail Varbanov <mihail.varbanov@inabyte.com>
2024-12-04 13:24:36 +0200

    Do not cache pronunciation audio [rokwire/illinois-app#4402].

--- lib/service/auth2.dart
+++ lib/service/auth2.dart
@@ -1230,7 +1230,7 @@ class Auth2 with Service, NetworkAuthProvider implements NotificationsListener {
 
   Future<bool> saveUserProfile(Auth2UserProfile? profile) async {
     if (await _saveUserProfile(profile)) {
-      if (_account?.profile?.apply(profile) ?? false) {
+      if (_account?.profile?.apply(profile, scope: Auth2UserProfileScopeImpl.fullScope) ?? false) {
         Storage().auth2Account = _account;
         NotificationService().notify(notifyProfileChanged);
       }
