5d016f893edfa705b20222b828d9204dc2e6806e
Ryan Oberlander <reoberlander2@gmail.com>
2025-02-12 19:25:29 -0600

    change Content().loadContentItems to POST, fix FileImpl.saveDownload

--- lib/platform_impl/mobile.dart
+++ lib/platform_impl/mobile.dart
@@ -55,14 +55,16 @@ class PasskeyImpl extends BasePasskey {
 class FileImpl extends BaseFile {
   Future<bool> saveDownload(String name, Uint8List data) async {
     if (Platform.isAndroid) {
-      Directory downloadsDir = Directory("/storage/emulated/0/Download");
+      Directory? downloadsDir = Directory("/storage/emulated/0/Download");
       if (!await downloadsDir.exists()) {
         downloadsDir = await getExternalStorageDirectory();
       }
-      File downloadsFile = File('${downloadsDir.path}/$name');
-      downloadsFile = await downloadsFile.writeAsBytes(data);
-      FileStat stats = await downloadsFile.stat();
-      return stats.size > 0;
+      if (downloadsDir != null) {
+        File downloadsFile = File('${downloadsDir.path}/$name');
+        downloadsFile = await downloadsFile.writeAsBytes(data);
+        FileStat stats = await downloadsFile.stat();
+        return stats.size > 0;
+      }
     }
     //TODO: implement for iOS
     return false;
