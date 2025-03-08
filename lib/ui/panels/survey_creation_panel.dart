068b537dcfae2c73a0b387f90f49756a4d690b62
Ryan Oberlander <reoberlander2@gmail.com>
2025-03-07 19:06:16 -0600

    fix some survey creation tool bugs

--- lib/ui/panels/survey_creation_panel.dart
+++ lib/ui/panels/survey_creation_panel.dart
@@ -596,7 +596,7 @@ class _SurveyCreationPanelState extends State<SurveyCreationPanel> {
   void _onTapEditRuleElement(RuleElement? element, SurveyElement surveyElement, {RuleElement? parentElement}) async {
     if (element != null) {
       RuleElement? ruleElement = await Navigator.push(context, CupertinoPageRoute(builder: (context) => RuleElementCreationPanel(
-        data: RuleElement.fromOther(element),
+        data: RuleElement.fromOther(element)..id = element.id,
         surveyElement: surveyElement,
         questionDataKeys: List.generate(_questionData.length, (index) => _questionData[index].key),
         questionDataTypes: List.generate(_questionData.length, (index) => _questionData[index].type),
