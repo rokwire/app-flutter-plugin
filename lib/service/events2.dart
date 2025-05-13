f167a3700a77c4bc39b8090938c7820d4ac3687c
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2025-05-13 13:29:15 +0300

    Introduce new API for creating group events [#602]

--- lib/service/events2.dart
+++ lib/service/events2.dart
@@ -218,6 +218,31 @@ class Events2 with Service, NotificationsListener {
     return null;
   }
 
+  ///
+  /// Creates group event for group admins that are not calendar admins
+  ///
+  /// Returns Event2 in case of success, String description in case of error
+  ///
+  Future<dynamic> createGroupEvent(Event2 source, {List<Event2PersonIdentifier>? adminIdentifiers}) async {
+    if (Config().calendarUrl != null) {
+      String url = "${Config().calendarUrl}/group/event";
+      String? body = JsonUtils.encode({
+        "event": source.toJson(),
+        "admins_identifiers": Event2PersonIdentifier.listToNotNullJson(adminIdentifiers) ?? []
+      });
+      Response? response = await Network().post(url, body: body, headers: _jsonHeaders, auth: Auth2());
+      if (response?.statusCode == 200) {
+        NotificationService().notify(notifyChanged);
+        _notifyGroupsForModifiedEvents(groupIds: source.groupIds);
+        return Event2.fromJson(JsonUtils.decodeMap(response?.body)?["event"]);
+      }
+      else {
+        return response?.errorText;
+      }
+    }
+    return null;
+  }
+
   // Returns Event2 in case of success, String description in case of error
   Future<dynamic> updateEvent(Event2 source, {Set<String>? initialGroupIds, List<Event2PersonIdentifier>? adminIdentifiers}) async {
     if (Config().calendarUrl != null) {
