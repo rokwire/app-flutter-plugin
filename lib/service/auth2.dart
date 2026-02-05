0087a1669c2fce0e1570de8d1d4740149ad06657
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2026-02-05 15:57:35 +0200

    Remove warning.

--- lib/service/auth2.dart
+++ lib/service/auth2.dart
@@ -1520,7 +1520,7 @@ class Auth2 with Service, NetworkAuthProvider, NotificationsListener {
             onDeepLinkUri(Uri.tryParse(url));
           });
         } else if (await canLaunchUrlString(urlStr)) {
-          await launchUrlString(urlStr, mode: LaunchMode.platformDefault).catchError((e) { debugPrint(e.toString()); });
+          await launchUrlString(urlStr, mode: LaunchMode.platformDefault).catchError((e) { debugPrint(e.toString()); return false;});
         }
       }
     }
